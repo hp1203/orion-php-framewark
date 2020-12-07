9fc5feda685f1d5a0c867da7d04b78dd03aa4d6f
Himanshu Purohit <himanshu1203@gmail.com>
2020-12-08 00:47:53 +0530

    implemented routing

--- core/Router.php
+++ core/Router.php
@@ -9,9 +9,38 @@ namespace app\core;
  */
 class Router 
 {
+    public Request $request;
+    protected array $routes = [];
+
+    /**
+     * Router Constructor.
+     * 
+     * @param app\core\Request $request
+     */
+
+    public function __construct(\app\core\Request $request)
+    {
+        $this->request = $request;
+    }
 
     public function get($path, $callback)
     {
-        # code...
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();
+        // echo "<pre>";
+        // var_dump($this->routes);
+        // echo "</pre>";
+        // exit;
+        $callback = $this->routes[$method][$path] ?? false;
+        if($callback === false){
+            echo "Not Found";
+            exit;
+        }
+        echo call_user_func($callback);
     }
 }
--- index.php
+++ index.php
@@ -5,8 +5,12 @@ use app\core\Application;
 
 $app = new Application();
 
-$app->router-get('/', function (){
+$app->router->get('/', function (){
     return 'Hello World!';
+});
+
+$app->router->get('/contact', function (){
+    return 'Contact';
 });
 
 $app->run();
